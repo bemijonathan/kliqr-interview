1150944158ab6d8e64573aae75e961c7da1af8c5
bemijonathan <bemijonathan@gmail.com>
2021-04-14 15:24:35 +0100

    fixed api gateway url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const request = axios.create({
     // baseURL: 'http://localhost:4000/api/v1/',
-    baseURL:'https://kliqr-proxy-service.herokuapp.com/',
+    baseURL:'https://kliqr-proxy-service.herokuapp.com/api/v1',
     headers: {
         token: 'YXJxZXRnbWVybGtwMzIjNTQjJUA='
     }
