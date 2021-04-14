63c0ff5305a88bf4d088653836ce4830d6fd77f2
bemijonathan <bemijonathan@gmail.com>
2021-04-14 15:18:40 +0100

    added cors

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'http://localhost:4000/api/v1/',
+    // baseURL: 'http://localhost:4000/api/v1/',
+    baseURL:'https://kliqr-proxy-service.herokuapp.com/',
     headers: {
         token: 'YXJxZXRnbWVybGtwMzIjNTQjJUA='
     }
